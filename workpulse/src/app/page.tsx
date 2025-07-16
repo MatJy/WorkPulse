4d5bcd787222bb11f46b3b850ab3d96156f2b8e2
Matias <matias.ta.jylha@gmail.com>
2025-07-16 12:25:11 +0300

    react next.js app structure

---
+++ workpulse/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div>
+            {' '}
+            <p>hello</p>
+            <a href="/register">register</a>
+            <br />
+            <a href="/login">login</a>
+        </div>
+    );
+}
